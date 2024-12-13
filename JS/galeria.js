344f516e5bb5da5896a86177e26d10824524a8d2
Jealm78 <jealm78@gmail.com>
2024-12-13 08:36:06 -0600

    cambio

---
+++ JS/galeria.js
@@ -0,0 +1,8 @@
+import jsonGaleria from '../Datos/galeria.json' assert { type: 'json' };
+console.log(jsonGaleria);
+
+function crearImagenes() {
+    console.log("crear imagenes");
+}
+
+crearImagenes();
